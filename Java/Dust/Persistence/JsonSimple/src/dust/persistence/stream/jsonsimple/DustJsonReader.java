8e976fed2d7c1b141380c0dcd5c6fb20f12b203d
lkedves <lorand.kedves@gmail.com>
2018-03-27 11:26:20 +0200

    JSON reading - promising mock callouts

--- Java/Dust/Persistence/JsonSimple/src/dust/persistence/stream/jsonsimple/DustJsonReader.java
+++ Java/Dust/Persistence/JsonSimple/src/dust/persistence/stream/jsonsimple/DustJsonReader.java
@@ -87,7 +87,7 @@ public class DustJsonReader implements DustJsonComponents, DustCoreExecServices.
 
 	class MetaInfo {
 		EnumMap<JsonTag, String> info = new EnumMap<>(JsonTag.class);
-		Map<String, Enum<?>> attrMap = new TreeMap<>();
+		Map<String, MetaInfo> attrMap = new TreeMap<>();
 	}
 
 	class ReadHandler implements ContentHandler {
@@ -103,6 +103,12 @@ public class DustJsonReader implements DustJsonComponents, DustCoreExecServices.
 		};
 
 		MetaInfo metaInfo = null;
+		MetaInfo metaAtt = null;
+		
+		String eIdLocal;
+		String eIdGlobal;
+		
+		DustEntity eTarget;
 
 		@Override
 		public void startJSON() throws ParseException, IOException {
@@ -115,21 +121,56 @@ public class DustJsonReader implements DustJsonComponents, DustCoreExecServices.
 
 		@Override
 		public boolean startObject() throws ParseException, IOException {
+			switch ( state ) {
+			case MetaExt:
+				metaInfo = new MetaInfo();
+				break;
+			case Models:
+				if ( null == eTarget ) {
+					eTarget = accessEntity(eIdGlobal);
+					mapEntities.put(eIdLocal, eTarget);
+				} else {
+					
+				}
+				break;
+			default:
+				break;
+			}
 			return true;
 		}
 
 		@Override
 		public boolean endObject() throws ParseException, IOException {
+			String alias, owner;
+			switch ( state ) {
+			case MetaExt:
+				alias = metaInfo.info.get(JsonTag.alias);
+				owner = metaInfo.info.get(JsonTag.ownerType);
+				
+				if ( null == owner ) {
+					metaMap.get(alias).info.putAll(metaInfo.info);
+				} else {
+					metaMap.get(owner).attrMap.put(alias, metaInfo);
+				}
+				metaInfo = null;
+				break;
+			default:
+				break;
+			}
 			return true;
 		}
 
 		@Override
 		public boolean startObjectEntry(String key) throws ParseException, IOException {
 			if ( JsonTag.Models == state ) {
-				
+				if ( null == metaInfo ) {
+					metaInfo = metaMap.peek(key);
+				} else {
+					metaAtt = metaInfo.attrMap.get(key);
+				}
 			} else {
 				state = (JsonTag) DustUtilsJava.parseEnum(key, JsonTag.class);
-				DustUtilsDev.dump(state);
+//				DustUtilsDev.dump(state);
 			}
 			return true;
 		}
@@ -137,7 +178,16 @@ public class DustJsonReader implements DustJsonComponents, DustCoreExecServices.
 		@Override
 		public boolean endObjectEntry() throws ParseException, IOException {
 			if ( JsonTag.Models == state ) {
-				
+				if ( null == metaAtt) {
+					if ( null == metaInfo) {
+						eTarget = null;
+						state = JsonTag.Entities;
+					} else {
+						metaInfo = null;
+					}
+				} else {
+					metaAtt = null;
+				}
 			} else {
 				state = state.parent;
 			}			
@@ -158,7 +208,7 @@ public class DustJsonReader implements DustJsonComponents, DustCoreExecServices.
 		public boolean primitive(Object value) throws ParseException, IOException {
 			try {
 				switch ( state ) {
-				case idLocal:
+				case idStore:
 					storeId = (String) value;
 					break;
 				case handler:
@@ -171,23 +221,21 @@ public class DustJsonReader implements DustJsonComponents, DustCoreExecServices.
 						DustException.throwException();
 					}
 					break;
-				case DustStreamInfo:
+				case alias:
+				case globalId:
+				case ownerType:
+					metaInfo.info.put(state, DustUtilsJava.toString(value));
 					break;
-				case Entities:
+				case idGlobal:
+					eIdGlobal = DustUtilsJava.toString(value);
 					break;
-				case MetaExt:
+				case idLocal:
+					eIdLocal = DustUtilsJava.toString(value);
 					break;
 				case Models:
-					break;
-				case Root:
-					break;
-				case StoreInfo:
-					break;
-				case alias:
-					break;
-				case globalId:
-					break;
-				case ownerType:
+					if ( null != metaAtt ) {
+						setField(eTarget, metaAtt, value);
+					}
 					break;
 				default:
 					break;
@@ -203,5 +251,14 @@ public class DustJsonReader implements DustJsonComponents, DustCoreExecServices.
 	}
 	
 	ReadHandler h = new ReadHandler();
+	
+	DustEntity accessEntity(String idGlobal) {
+		DustUtilsDev.dump("accessing entity", idGlobal);
+		return DustAttributeCoreDataEntity.svcImpl;
+	}
+
+	void setField(DustEntity e, MetaInfo mi, Object value) {
+		DustUtilsDev.dump("set field value", mi.info.get(JsonTag.globalId), value);
+	}
 
 }
