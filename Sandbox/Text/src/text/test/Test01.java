5764d8a1a4092f4994165a779616b2fda0b1d898
Lorand Kedves <lorand.kedves@gmail.com>
2018-05-26 05:42:55 +0200

    Text management (initial)

---
+++ Sandbox/Text/src/text/test/Test01.java
@@ -0,0 +1,165 @@
+package text.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Test01 implements Test01Constants {
+	
+	public static void main(String[] args) throws IOException {
+//		String fname = "input/61_2009_IRM.html.txt";
+		String fname = "input/Ptk.html.txt";
+//		String fname = "input/61_2009_IRM.html.txt";
+//		String fname = "input/61_2009_IRM.html.txt";
+		
+		Document doc = Jsoup.parse(new File(fname), "UTF-8");
+		Elements list;
+		Element e;
+		String txt;
+		Matcher m;
+		int l;
+		
+		list = doc.getElementsByAttribute("data-tid");
+		l = list.size();
+
+//		l = 100;
+
+		for (int i = 0; i < l; ++i) {
+			e = list.get(i);
+			if (!printElementHeader(e)) {
+				txt = e.text();
+				m = PT_PARA.matcher(txt);
+				if ( m.matches() ) {
+					dump(">>>", PGrp.para.get(m), "paragrafus", PGrp.text.get(m));
+				}
+			}
+		}
+
+		list = doc.getElementsByClass("lbjvallist");
+		l = list.size();
+		
+		Set<String> tvRefs = new TreeSet<>();
+
+		if (1 == l) {
+			dump("==== Footer ====");
+			e = list.first();
+			String from;
+			String to;
+			StringBuilder sb = new StringBuilder();
+			
+			for (Element n : e.children()) {
+				txt = n.text();
+//				txt = txt.replace("),", ");").replace("§,", "§;");
+				from = to = "-";
+				
+				m = PT_MOD_HAT.matcher(txt);
+				if ( m.matches() ) {
+					from = PGrp.date.get(m);
+					txt = PGrp.text.get(m);
+				} else {
+					m = PT_MOD_HATKIV.matcher(txt);
+					if ( m.matches() ) {
+						to = PGrp.date.get(m);
+						txt = PGrp.text.get(m);
+					}
+				}
+				
+				Change chg = Change.fromText(txt, sb);
+				if ( null != chg ) {
+					String head = chg.name();
+					for ( String ref : sb.toString().split(",")) {
+						m = PT_REF.matcher(ref);
+						if ( m.matches() ) {
+							String law = PGrp.refLaw.get(m).trim();
+							tvRefs.add(law);
+							dump(head,  "(", law, "->", PGrp.refLoc.get(m), ")", "(", from, "/", to, ")");							
+						} else {
+							dump(head,  "BAD REF", ref, "(", from, "/", to, ")");														
+							dump("dump", n.text());
+						}
+						head = "  ";
+					}
+					
+				} else {
+					dump("cannot process", n.text());
+				}
+			}
+			
+			dump("Referred laws");
+			for ( String law : tvRefs ) {
+				dump("  ", law);
+			}
+		}
+	}
+
+	public static void dump(Object... objects) throws IOException {
+		for (Object o : objects) {
+			System.out.print(o);
+			System.out.print(" ");
+		}
+		System.out.println();
+	}
+
+	public static boolean printElementHeader(Element e) throws IOException {
+		if (e.classNames().contains("agc")) {
+			String tn = e.tagName();
+			if (tn.startsWith("h")) {
+				int lvl = Integer.parseInt(tn.substring(1));
+				System.out.print(lvl);
+				for (int in = 0; in < lvl; ++in) {
+					System.out.print(" ");
+				}
+				String txt = e.text();
+				// if ( txt.startsWith("49.")) {
+				// printElementDump(e);
+				// }
+				System.out.println(txt);
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public static void checkA(Document doc) throws IOException {
+		Elements list;
+		Element e;
+		int l;
+		
+		list = doc.getElementsByTag("a");
+		l = list.size();
+		
+		for (int i = 0; i < l; ++i) {
+			e = list.get(i);
+//			if ( !e.classNames().contains("lbjlink") ) 
+			{
+				dump(e.text());
+			}
+		}
+	}
+
+	public static void readDoc(Document doc) throws IOException {
+		Elements nodelist = doc.getElementsByTag("body");
+
+		if (nodelist != null) {
+			org.jsoup.nodes.Element body = nodelist.first();
+			List<org.jsoup.nodes.Node> bodyChildren = body.childNodes();
+
+			int len = bodyChildren.size();
+			for (int i = 0; i < len; i++) {
+				String ml = bodyChildren.get(i).toString();
+				if (ml != null) {
+					System.out.println(ml);
+				}
+			}
+		}
+	}
+}
---
+++ Sandbox/Text/src/text/test/Test01Constants.java
@@ -0,0 +1,55 @@
+package text.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public interface Test01Constants {
+	enum PGrp {
+		para, text, date,
+		year, month, day,
+		refLaw, refLoc;
+		
+		public String toString() {
+			return "?<" + name() + ">";
+		}
+		
+		public String get(Matcher m) {
+			return m.group(name());
+		}
+	}
+	
+	enum Change {
+		Create("Megállapította: "), Change("Módosította: "), Remove("Hatályon kívül helyezte: "), Insert("Beiktatta: "), 
+		;
+		
+		private final String header;
+
+		private Change(String header) {
+			this.header = header;
+		}
+		
+		public static Change fromText(String str, StringBuilder content) {
+			for ( Change c : values()) {
+				if ( str.startsWith(c.header)) {
+					content.replace(0, content.length(), str.substring(c.header.length()));
+					return c;
+				}
+			}
+			content.replace(0, content.length(), str);
+			return null;
+		}
+		
+	}
+
+	public static String ptGroup(String grp) {
+		return "";
+	}
+	
+	Pattern PT_PARA = Pattern.compile("^(" + PGrp.para + "(\\d*\\.)*)\\s*§\\s*(" + PGrp.text + ".*)");
+	Pattern PT_DATE = Pattern.compile("(" + PGrp.year + "\\d{4})\\.\\s*(" + PGrp.month + "[XIV]*).\\s*(" + PGrp.day + "\\d*)\\.*");
+	Pattern PT_REF = Pattern.compile("(" + PGrp.refLaw + ".*)\\s+(" + PGrp.refLoc + "(\\d+\\.)+\\s*§\\s*.*)");
+	
+	Pattern PT_MOD_HAT = Pattern.compile("^(" + PGrp.text + ".*)\\s*Hatályos:\\s*(" + PGrp.date + ".*)-.*");
+	Pattern PT_MOD_HATKIV = Pattern.compile("^(" + PGrp.text + ".*)\\s*Hatálytalan:\\s*(" + PGrp.date + ".*)-.*");
+
+}
